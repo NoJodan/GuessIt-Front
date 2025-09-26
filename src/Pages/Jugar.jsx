41ca650628a79f29e8727c3afca38e5f7027d710
cry <cfcarmonac@ut.edu.co>
2025-09-25 23:40:32 -0500

    Se realizo Jugar con base en una de las paginar de login y sign up, se creo jugar.css vacio para su posterior uso

--- src/Pages/Jugar.jsx
+++ src/Pages/Jugar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../styles/jugar.css'
 
 function Jugar() {
   return (
-    <div>Jugar</div>
+    <div className="jugar-page">
+      <header className="panel-jugar">
+        <img className="MiniLogo" src="/MiniLogo.png" alt="Mini logo Guess It!!" width="500px" height="auto"/>
+      </header>
+      <div className="jugar-card">
+        <h2 className="jugar-title">Modo de Juego</h2>
+
+        <form className="jugar-form">
+          <button type="button" className="btn-jugar">Un Jugador</button>
+          <button type="button" className="btn-jugar">Dos Jugadores</button>
+        </form>
+
+        <p className="jugar-footer">
+          ¡Elige tu modo de juego y diviértete!
+        </p>
+      </div>
+    </div>
+
   )
 }
 
