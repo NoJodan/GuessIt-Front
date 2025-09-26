755f6ec76610d74715746a95c55f84ef650313ff
cry <cfcarmonac@ut.edu.co>
2025-09-26 00:31:25 -0500

    Se añadio un modulo para archivos Json, en este esta almacenado juegos.json, en el cual estan almacenados todos los distintos m odos de juego, dicho modulo se inicializa al inicio de la funcion jugar en .jsx, en donde se recrea para que se añadan de forma dinamica si haci lo realiza el admin

--- src/Pages/Jugar.jsx
+++ src/Pages/Jugar.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import juegosConfig from "../config/juegos.json";
 import '../styles/jugar.css'
 
 function Jugar() {
+  // Inicializa el estado 'juegos' con un array vacío.
+  // Al montar el componente, se carga la configuración desde el JSON usando useEffect, 
+  // recordar se implemento el json para la configuracion de los juegos
+  // y se actualiza el estado 'juegos' con los datos importados.
+  // Esto permite que el componente tenga acceso a la lista de juegos para renderizarlos dinámicamente.
+  const [juegos, setJuegos] = useState([]);
+  useEffect(() => {
+    setJuegos(juegosConfig);
+  }, []);
   return (
     <div className="jugar-page">
       <header className="panel-jugar">
-        <img className="MiniLogo" src="/MiniLogo.png" alt="Mini logo Guess It!!" width="500px" height="auto"/>
+        <img
+          className="MiniLogo"
+          src="/MiniLogo.png"
+          alt="Mini logo Guess It!!"
+          width="500px"
+          height="auto"
+        />
       </header>
-      <div className="jugar-card">
-        <h2 className="jugar-title">Modo de Juego</h2>
 
+      <div className="jugar-card">
+        <h2 className="jugar-title">Selecciona tu Juego</h2>
         <form className="jugar-form">
-          <button type="button" className="btn-jugar">Un Jugador</button>
-          <button type="button" className="btn-jugar">Dos Jugadores</button>
+          {juegos.map((juego) => (
+            <button
+              key={juego.id}
+              type="button"
+              className="btn-jugar"
+              onClick={() => alert(`Entrar a ${juego.nombre}`)}
+            >
+              {juego.nombre}
+            </button>
+          ))}
         </form>
 
         <p className="jugar-footer">
-          ¡Elige tu modo de juego y diviértete!
+          ¡Elige un juego y diviértete!
         </p>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Jugar
+export default Jugar;
