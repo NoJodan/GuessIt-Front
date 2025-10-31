a164f1636d79bef70611a1d68236736fc71aacda
Lawliet <48108694+L4wl13t-0@users.noreply.github.com>
2025-10-30 23:07:42 -0500

    [FIX] Se corrige la conexion

--- src/Pages/Jugar.jsx
+++ src/Pages/Jugar.jsx
@@ -16,7 +16,7 @@ function Jugar() {
   const getAllThemes = async () => {
     try {
       const token = localStorage.getItem("token");
-      const response = await fetch("https://zooming-integrity-production-6c7d.up.railway.app/api/game/themes", {
+      const response = await fetch("https://zooming-integrity.railway.internal/api/game/themes", {
         headers: { Authorization: `Bearer ${token}` },
       });
       const data = await response.json();
